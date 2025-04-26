446e77d47d703a1223b9d34b636c1e349eff3c92
WebsiteProffesional <gamingland1318@gmail.com>
2025-04-27 03:21:21 +0500

    Update page.js

--- app/user/[user]/dashboard/page.js
+++ app/user/[user]/dashboard/page.js
@@ -4,7 +4,7 @@ export default async function Page({ params }) {
     return (
       <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
         <h1 className="text-4xl font-bold mb-4">Welcome </h1>
-        <p className="text-lg">This is your dashboard on which we are working and very soon you will be able to see you data here.</p>
+        <p className="text-lg">This is your dashboard on which we are working and very soon you will be able to see your data here.</p>
       </div>
     );
   }
