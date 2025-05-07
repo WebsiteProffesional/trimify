38e406e65885c77e036590799c888c306b0236e5
WebsiteProffesional <gamingland1318@gmail.com>
2025-05-08 03:29:36 +0500

    Delete app directory

--- app/components/Loader.js
+++
@@ -1,9 +0,0 @@
-// Loader.js
-export default function Loader() {
-    return (
-      <div className="flex justify-center items-center mt-2 ">
-        <div className="w-12 h-12  border-4 border-t-4 border-blue-600 rounded-full animate-spin"></div>
-      </div>
-    );
-  }
-  
