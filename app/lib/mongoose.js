72af7673e22bef82014ec51cd64a409aba397af0
WebsiteProffesional <gamingland1318@gmail.com>
2025-05-08 19:09:27 +0500

    Add files via upload

---
+++ app/lib/mongoose.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+  throw new Error("Please define the MONGO_URI in your .env.local file");
+}
+
+let cached = global.mongoose || { conn: null, promise: null };
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+global.mongoose = cached;
+
+export default connectDB;
