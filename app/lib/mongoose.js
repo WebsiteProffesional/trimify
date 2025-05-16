fcf4c400f104c24dcb580636e5d1053a03446a36
WebsiteProffesional <gamingland1318@gmail.com>
2025-05-17 02:31:31 +0500

    Update mongoose.js

--- app/lib/mongoose.js
+++ app/lib/mongoose.js
@@ -11,10 +11,7 @@ let cached = global.mongoose || { conn: null, promise: null };
 async function connectDB() {
   if (cached.conn) return cached.conn;
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      
-    });
+    cached.promise = mongoose.connect(MONGO_URI)
   }
   cached.conn = await cached.promise;
   return cached.conn;
