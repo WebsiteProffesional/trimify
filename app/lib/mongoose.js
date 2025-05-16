d1823ee215c3d5a1d2559c0a42491aa64ee02c6a
WebsiteProffesional <gamingland1318@gmail.com>
2025-05-17 02:35:34 +0500

    Update mongoose.js

--- app/lib/mongoose.js
+++ app/lib/mongoose.js
@@ -12,6 +12,7 @@ async function connectDB() {
   if (cached.conn) return cached.conn;
   if (!cached.promise) {
     cached.promise = mongoose.connect(MONGO_URI)
+     console.log('MongoDB connected successfully');
   }
   cached.conn = await cached.promise;
   return cached.conn;
