58d422d4755a010cdb1959d73d0a3b7d83be908a
WebsiteProffesional <gamingland1318@gmail.com>
2025-05-30 01:56:26 +0500

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,16 +1,16 @@
 import Login from "../components/login"
 export const metadata = {
-  title: "Login to Trimify - The Ultimate URL Shortner",
+  title: "Login to Trimify - The Ultimate URL Shortener ",
   description: "Log in to your Trimify account to access your dashboard and to create custom short links.If you have not created your account till now then first go and create your account.",
   alternates: {
     canonical: "/login"
   },
    openGraph: {
-   title: "Login to Trimify - The Ultimate URL Shortner",
+   title: "Login to Trimify - The Ultimate URL Shortener   ",
    description:
       "Log in to your Trimify account to access your dashboard and to create custom short links.If you have not created your account till now then first go and create your account.",
     url: "/login",
-    siteName: "Trimify - The Ultimate URL Shortner",
+    siteName: "Trimify - The Ultimate URL Shortener ",
     images: [
       {
         url: "/og-image.png", // Recommended: 1200x630 og-image instead of favicon
@@ -24,7 +24,7 @@ export const metadata = {
   },
     twitter: {
     card: "summary_large_image",
-    title: "Login to Trimify - The Ultimate URL Shortner",
+    title: "Login to Trimify - The Ultimate URL ",
     description: "Log in to your Trimify account to access your dashboard and to create custom short links.If you have not created your account till now then first go and create your account.",
     images: ["/og-image.png"],
   },
