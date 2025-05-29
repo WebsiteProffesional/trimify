0651c5d1357ecf304acdbe69d9310ccc75717a2f
WebsiteProffesional <gamingland1318@gmail.com>
2025-05-30 01:36:58 +0500

    Update page.js

--- app/generate-shortUrl/page.js
+++ app/generate-shortUrl/page.js
@@ -4,7 +4,30 @@ export const metadata = {
   description: "Paste your long URL and generate a short link instantly using Trimify.",
   alternates: {
     canonical: "/generate-shortUrl"
-  }
+  },
+   openGraph: {
+   title: "Generate Short URLs for Free - Trimify",
+   description:
+      "Paste your long URL and generate a short link instantly using Trimify.",
+    url: "/generate-shortUrl",
+    siteName: "Trimify",
+    images: [
+      {
+        url: "/og-image.png", // Recommended: 1200x630 og-image instead of favicon
+        width: 800,
+        height: 600,
+        alt: "Trimify logo",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+    twitter: {
+    card: "summary_large_image",
+    title: "Generate Short URLs for Free - Trimify",
+    description: "Paste your long URL and generate a short link instantly using Trimify.",
+    images: ["/og-image.png"],
+  },
 }
 const page = () => {
   return (
