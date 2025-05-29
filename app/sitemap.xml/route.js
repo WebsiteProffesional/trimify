2ae44ccc379438f26b937f56d7e938d614d800bc
WebsiteProffesional <gamingland1318@gmail.com>
2025-05-29 15:39:13 +0500

    Update route.js

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,13 +1,40 @@
 export async function GET() {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
-  <url><loc>https://trimify.xyz/</loc><priority>1.0</priority></url>
-  <url><loc>https://trimify.xyz/generate-shortUrl</loc><priority>0.9</priority></url>
-  <url><loc>https://trimify.xyz/login</loc><priority>0.8</priority></url>
-  <url><loc>https://trimify.xyz/sign-up-to-trimify</loc><priority>0.8</priority></url>
-  <url><loc>https://trimify.xyz/count-clicks</loc><priority>0.7</priority></url>
-  <url><loc>https://trimify.xyz/about</loc><priority>0.7</priority></url>
-  <url><loc>https://trimify.xyz/contact</loc><priority>0.7</priority></url>
+  <url>
+    <loc>https://www.trimify.xyz/</loc>
+    <priority>1.0</priority>
+  </url>
+
+  <url>
+    <loc>https://www.trimify.xyz/generate-shortUrl</loc>
+    <priority>0.9</priority>
+  </url>
+
+  <url>
+    <loc>https://www.trimify.xyz/login</loc>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>https://www.trimify.xyz/sign-up-to-trimify</loc>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>https://www.trimify.xyz/count-clicks</loc>
+    <priority>0.7</priority>
+  </url>
+
+  <url>
+    <loc>https://www.trimify.xyz/about</loc>
+    <priority>0.7</priority>
+  </url>
+
+  <url>
+    <loc>https://www.trimify.xyz/contact</loc>
+    <priority>0.7</priority>
+  </url>
 </urlset>`;
 
   return new Response(sitemap, {
