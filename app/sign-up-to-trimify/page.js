81ad4698ab04a12f4b365feaba3b5d3528606b2c
WebsiteProffesional <gamingland1318@gmail.com>
2025-05-30 01:13:30 +0500

    Update page.js

--- app/sign-up-to-trimify/page.js
+++ app/sign-up-to-trimify/page.js
@@ -5,20 +5,20 @@ export const metadata = {
   description: "Create your Trimify account to shorten and manage links with analytics.Also get a seperate dashboard for url management.",
   alternates: {
     canonical: "/sign-up-to-trimify"
-  }
+  },
   openGraph: {
     title: "Sign up to Trimify - The Ultimate URL Shortner",
     description:
       "Create your Trimify account to shorten and manage links with analytics.Also get a seperate dashboard for url management.",
     url: "/",
-  }
+  },
     twitter: {
     card: "summary_large_image",
     title: "Sign up to Trimify - The Ultimate URL Shortner",
     description:
       "Create your Trimify account to shorten and manage links with analytics.Also get a seperate dashboard for url management.",
-  }
-}
+  },
+};
 const page = () => {
   return (
     <div>
