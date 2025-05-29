40d9efae5dec792d823cb9bca13a3bf15f9b0734
WebsiteProffesional <gamingland1318@gmail.com>
2025-05-30 01:53:33 +0500

    Update page.js

--- app/sign-up-to-trimify/page.js
+++ app/sign-up-to-trimify/page.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Signup from "../components/Signup"
 export const metadata = {
-  title: "Sign up to Trimify - The Ultimate URL Shortner",
+  title: "Sign up to Trimify - The Ultimate URL Shortener",
   description: "Create your Trimify account to shorten and manage links with analytics.Also get a seperate dashboard for url management.",
   alternates: {
     canonical: "/sign-up-to-trimify"
   },
   openGraph: {
-   title: "Sign up to Trimify - The Ultimate URL Shortner",
+   title: "Sign up to Trimify - The Ultimate URL Shortener",
    description:
       "Create your Trimify account to shorten and manage links with analytics.Also get a seperate dashboard for url management.",
     url: "/sign-up-to-trimify",
@@ -25,7 +25,7 @@ export const metadata = {
   },
     twitter: {
     card: "summary_large_image",
-    title: "Sign up to Trimify - The Ultimate URL Shortner",
+    title: "Sign up to Trimify - The Ultimate URL Shortener",
     description: "Create your Trimify account to shorten and manage links with analytics.Also get a seperate dashboard for url management.",
     images: ["/og-image.png"],
   },
