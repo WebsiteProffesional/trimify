db9f738246efbaa685828ac6b84796d375c9b35a
WebsiteProffesional <gamingland1318@gmail.com>
2025-05-29 14:32:41 +0500

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -7,6 +7,6 @@ export default function robots() {
         disallow: ['/user/*'],
       },
     ],
-    sitemap: 'https://acme.com/sitemap.xml',
+    sitemap: 'https://trimify.xyz/sitemap.xml',
   }
 }
