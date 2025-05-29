7bcf027fcd5feb52046c71602a6ae7a8f0a076d4
WebsiteProffesional <gamingland1318@gmail.com>
2025-05-30 01:39:18 +0500

    Update page.js

--- app/count-clicks/page.js
+++ app/count-clicks/page.js
@@ -1,10 +1,33 @@
 import Counter from "../components/Counter"
 export const metadata = {
-  title: "Count Clicks on URL – Trimify",
+  title: "Count Clicks on Short URL – Trimify",
   description: "Track how many times your shortened URL was clicked using Trimify's analytics.",
   alternates: {
     canonical: "/count-clicks"
-  }
+  },
+   openGraph: {
+   title: "Count Clicks on Short URL – Trimify",
+   description:
+      "Track how many times your shortened URL was clicked using Trimify's analytics.",
+    url: "/count-clicks",
+    siteName: "Trimify",
+    images: [
+      {
+        url: "/og-image.png", // Recommended: 1200x630 og-image instead of favicon
+        width: 800,
+        height: 600,
+        alt: "Trimify logo",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+    twitter: {
+    card: "summary_large_image",
+    title: "Count Clicks on Short URL – Trimify",
+    description: "Track how many times your shortened URL was clicked using Trimify's analytics.",
+    images: ["/og-image.png"],
+  },
 }
 const page = () => {
   return (
