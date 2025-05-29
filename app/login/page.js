cf430372322859748400996694ce693b1a31fdc9
WebsiteProffesional <gamingland1318@gmail.com>
2025-05-30 01:34:00 +0500

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,11 +1,33 @@
 import Login from "../components/login"
 export const metadata = {
   title: "Login to Trimify - The Ultimate URL Shortner",
-  description: "Log in to your Trimify account to access your dashboard and to create custom short links.",
+  description: "Log in to your Trimify account to access your dashboard and to create custom short links.If you have not created your account till now then first go and create your account.",
   alternates: {
     canonical: "/login"
-  }
-  
+  },
+   openGraph: {
+   title: "Login to Trimify - The Ultimate URL Shortner",
+   description:
+      "Log in to your Trimify account to access your dashboard and to create custom short links.If you have not created your account till now then first go and create your account.",
+    url: "/login",
+    siteName: "Trimify - The Ultimate URL Shortner",
+    images: [
+      {
+        url: "/og-image.png", // Recommended: 1200x630 og-image instead of favicon
+        width: 800,
+        height: 600,
+        alt: "Trimify logo",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+    twitter: {
+    card: "summary_large_image",
+    title: "Login to Trimify - The Ultimate URL Shortner",
+    description: "Log in to your Trimify account to access your dashboard and to create custom short links.If you have not created your account till now then first go and create your account.",
+    images: ["/og-image.png"],
+  },
 }
 
 const page = () => {
