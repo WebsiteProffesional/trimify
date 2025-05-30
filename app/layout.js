a0a4e0b32e4ddc2735207814c5719ec5e954f7cf
WebsiteProffesional <gamingland1318@gmail.com>
2025-05-30 19:53:13 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -24,13 +24,7 @@ export default function RootLayout({ children }) {
        <link rel="icon" href="/favicon.png" type="image/png" />
        <meta name="yandex-verification" content="856eed846c4fcea9" />
        <meta name="google-adsense-account" content="ca-pub-6762794271004506" />   
-       <Script
-          src="https://fpyf8.com/88/tag.min.js"
-          data-zone="149911"
-          strategy="beforeInteractive" // Ensures it's in the <head> before page loads
-          async
-          data-cfasync="false"
-        />
+       <script src="https://fpyf8.com/88/tag.min.js" data-zone="149911" async data-cfasync="false"></script>
          {/* Google tag (gtag.js) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-6N1QFL1KLS"
