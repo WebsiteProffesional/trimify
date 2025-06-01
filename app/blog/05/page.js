61966ce097d5b8585631942db475ea580de466ed
WebsiteProffesional <gamingland1318@gmail.com>
2025-06-02 03:36:44 +0500

    Create page.js

---
+++ app/blog/05/page.js
@@ -0,0 +1,52 @@
+import React from 'react'
+export const metadata = {
+  keywords: [
+    "url shortener use cases",
+    "how to use url shorteners",
+    "url shortening benefits",
+    "url shortener examples",
+    "Trimify url shortener use cases",
+    "marketing url short links",
+    "social media url shortener",
+  ],
+  icons: {
+    icon: "/favicon.ico", // favicon
+  },
+  metadataBase: new URL("https://trimify.xyz"), // Replace with your domain later
+  alternates: {
+    canonical: "https://trimify.xyz/blog/05",
+  },
+  openGraph: {
+    title: "Use Case of URL Shorteners | Trimify-Blog",
+    description:
+      "Discover various use cases of URL shorteners across marketing, social media, analytics, and more. Learn how Trimify enhances link management and tracking for users.",
+    url: "https://trimify.xyz/blog/05",
+    siteName: "Trimify - The Ultimate and Free URL Shortener",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 800,
+        height: 600,
+        alt: "Trimify logo",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Use Case of URL Shorteners | Trimify-Blog",
+    description:
+      "Discover various use cases of URL shorteners across marketing, social media, analytics, and more. Learn how Trimify enhances link management and tracking for users.",
+    images: ["/og-image.png"],
+  },
+};
+const page = () => {
+  return (
+    <div className='min-h-[80.8vh] w-full flex justify-center '>
+      <h2 className='bg-gray-100 text-2xl bree-bold max-h-fit mt-60 p-4 rounded-lg'>Publishing Soon...</h2>
+    </div>
+  )
+}
+
+export default page
