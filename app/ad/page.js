f0945f0ecd7572d7b4d2a89ba0ed4b77c3268051
WebsiteProffesional <gamingland1318@gmail.com>
2025-06-03 03:05:34 +0500

    Update page.js

--- app/ad/page.js
+++ app/ad/page.js
@@ -1,44 +1,10 @@
-"use client"
-import { useEffect, useState } from "react";
-import { useSearchParams, useRouter } from "next/navigation";
-import Script from "next/script";
-export default function AdPage() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const finalUrl = searchParams.get("url");
-  const [timeLeft, setTimeLeft] = useState(5); // 5 seconds
+import React, { Suspense } from "react";
+import Ads from "@/app/components/Ads";
 
-  useEffect(() => {
-    if (timeLeft === 0) return;
-
-    const timer = setInterval(() => {
-      setTimeLeft((prev) => prev - 1);
-    }, 1000);
-
-    return () => clearInterval(timer); // cleanup on unmount
-  }, [timeLeft]);
-  useEffect(() => {
-    // Show ad for 5 seconds, then redirect
-    const timer = setTimeout(() => {
-      window.location.href = finalUrl;
-    }, 5000); // 5 seconds
-
-    return () => clearTimeout(timer);
-  }, [finalUrl]);
-
+export default function Page() {
   return (
-    <>
-      <Script
-        id="monetag-ad-script"
-        src="https://fpyf8.com/88/tag.min.js"
-        strategy="afterInteractive"
-        async
-        data-zone="149911"
-        data-cfasync="false"
-      />
-      <div className="flex items-center justify-center h-screen bg-black text-white">
-        <h1 className="text-xl">Redirecting in {timeLeft}.Please wait</h1>
-      </div>
-    </>
+    <Suspense fallback={<div className="text-white bg-black h-screen flex items-center justify-center">Loading...</div>}>
+      <Ads />
+    </Suspense>
   );
 }
